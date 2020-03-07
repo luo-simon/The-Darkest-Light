6122923008fa9cde03d8ea46f86169f9cf8cef09
itsnotsimon <47718189+itsnotsimon@users.noreply.github.com>
2020-03-07 20:42:54 +0000

    Add compass

---
+++ Assets/Compass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    public float angle;
+
+    void Update()
+    {
+
+        angle = GetAngleFromVector(transform.parent.transform.position);
+        transform.eulerAngles = Vector3.forward * (angle + 90);
+    }
+
+    private float GetAngleFromVector(Vector2 vector)
+    {
+        // tan(a) = vector.y / vector.x
+        // a = arctan(vector.y / vector.x)
+        return Mathf.Atan2(vector.y, vector.x) * (180 / Mathf.PI);
+    }
+}
